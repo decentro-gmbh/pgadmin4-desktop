bf8abe89b30267647c526d1275dad153e9f2cc0a
Benjamin Assadsolimani <benjamin.assadsolimani@gmail.com>
2018-12-08 10:09:06 +0100

    disable upgrade check, set sqlite_path in config

--- config_local.py
+++ config_local.py
@@ -1,2 +1,8 @@
 # Turn off server mode (disables authentication)
 SERVER_MODE = False
+
+# Disable upgrade checks; container should be immutable
+UPGRADE_CHECK_ENABLED = False
+
+# Use copied over pgAdmin settings database
+SQLITE_PATH = "/var/lib/pgadmin/pgadmin4.db"
